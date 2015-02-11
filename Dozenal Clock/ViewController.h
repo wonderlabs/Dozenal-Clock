baabd9f3b7f72c366ea6e98b1c7025764aa7f44d
Drasko Vucenovic <vucenod@gmail.com>
2015-02-11 12:49:36 -0500

    Initial Commit

---
+++ Dozenal Clock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dozenal Clock
+//
+//  Created by Monica Brinkman on 2015-02-11.
+//  Copyright (c) 2015 Wonderlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
